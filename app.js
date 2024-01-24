a06fa8ab60d4abbe649d9fa58b28f97e0d663d72
Rocio Acosta <121262117+rooacost@users.noreply.github.com>
2024-01-23 22:15:47 -0300

    avance con bucle while

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 let numeroSecreto = 7;
 let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = "vez";
 
 
 while(numeroSecreto !== numeroUsuario){
   numeroUsuario = prompt('Me indicas un número entre 1 y 10, por favor:');
+  console.log(numeroUsuario);
+
   if(numeroSecreto == numeroUsuario){
-    alert(`Acertaste, el número es : ${numeroUsuario}`);
+    alert(`Acertaste, el número es : ${numeroUsuario}, lo hiciste en ${intentos} ${palabraVeces}`);
   }else{
     if (numeroUsuario > numeroSecreto) {
       alert('El numero secreto es menor');
     } else {
       alert('El numero secreto es mayor');
     }
+    //incrementamos el contador cuando no acierta
+    intentos = intentos + 1;
+    palabraVeces ='veces';
   }
-}
+};
---
+++ ejercicios/04_ejercicio.js
@@ -0,0 +1,25 @@
+//contador = 1;
+
+//sumar
+// while(contador < 11){
+//  console.log(`contador es igual a: ${contador}`);
+//  contador++;
+// }
+ 
+
+//decrementar
+// contador = 10;
+
+// while(contador >= 0 ){
+//  console.log(`Contador en decremento es igual a: ${contador}`);
+//  contador--;
+// }
+
+//cuenta progresiva
+ let numeroIngresado = prompt("Elije un número");
+ let numeroIncrementado = 0;
+
+ while(numeroIngresado >= numeroIncrementado){
+  console.log(`Cuenta regresiva: ${numeroIncrementado}`);
+  numeroIncrementado++;
+ }
